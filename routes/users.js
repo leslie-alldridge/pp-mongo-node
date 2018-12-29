3637de6ea17b2b4a604facef12ca062a2a2de8bc
leslie-alldridge <leslie.alldridge@gmail.com>
2018-12-30 09:43:05 +1300

    register and login

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/login", (req, res) => {
-  res.send("login");
+  res.render("login");
 });
 
 router.get("/register", (req, res) => {
-  res.send("register");
+  res.render("register");
 });
 
 module.exports = router;
