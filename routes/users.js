314956c7c3b05def605ce41874f8e81b1a78cc3b
leslie-alldridge <leslie.alldridge@gmail.com>
2018-12-30 10:12:32 +1300

    validation for form

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,21 @@ router.get("/register", (req, res) => {
   res.render("register");
 });
 
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+  // Check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please fill in all fields" });
+  }
+  //Check passwords match
+  if (password !== password2) {
+    errors.push({ msg: "Passwords do not match" });
+  }
+  //Check password length
+  if (password.length < 6) {
+    errors.push({ msg: "Password should be atleast six characters" });
+  }
+});
+
 module.exports = router;
