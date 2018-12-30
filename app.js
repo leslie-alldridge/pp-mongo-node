c4816a598c496a07037fcac796100a5074a319ff
leslie-alldridge <leslie.alldridge@gmail.com>
2018-12-30 15:20:20 +1300

    login flow done minus passport

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
 
@@ -22,6 +24,26 @@ app.set("view engine", "ejs");
 
 //Bodyparser
 app.use(express.urlencoded({ extended: false }));
+
+//Express session
+app.use(
+  session({
+    secret: "cats are awesome",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+//Connect flash
+app.use(flash());
+
+//Global variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 //Routes
 app.use("/", require("./routes/index"));
--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,7 @@ const bcrypt = require("bcryptjs");
 //User model
 const User = require("../models/User");
 
-router.get("/login", (req, res) => {
-  res.render("login");
-});
+router.get("/login", (req, res) => res.render("login"));
 
 router.get("/register", (req, res) => {
   res.render("register");
@@ -64,13 +62,35 @@ router.post("/register", (req, res) => {
             //save the user
             newUser
               .save()
-              .then(res.redirect("/users/login"))
+              .then(user => {
+                req.flash(
+                  "success_msg",
+                  "You are now registered and you can log in"
+                );
+                res.redirect("/users/login");
+              })
               .catch(err => console.log(err));
           })
         );
       }
     });
   }
+});
+
+// Login
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true
+  })(req, res, next);
+});
+
+// Logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/users/login");
 });
 
 module.exports = router;
