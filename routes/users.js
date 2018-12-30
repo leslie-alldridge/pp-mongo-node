f1c9f847bd1df4c38483224c597d139f1e6655cf
leslie-alldridge <leslie.alldridge@gmail.com>
2018-12-30 14:48:06 +1300

    users save to db

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+//User model
+const User = require("../models/User");
 
 router.get("/login", (req, res) => {
   res.render("login");
@@ -33,7 +37,39 @@ router.post("/register", (req, res) => {
       password2
     });
   } else {
-    res.send("passed form");
+    //Validation passed
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        //User exists
+        errors.push({ msg: "Email already in use" });
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+        //Hash password
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            //set password to the hashed one
+            newUser.password = hash;
+            //save the user
+            newUser
+              .save()
+              .then(res.redirect("/users/login"))
+              .catch(err => console.log(err));
+          })
+        );
+      }
+    });
   }
 });
 
